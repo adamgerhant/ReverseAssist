e3a94ea4edba25e3d23c51f628a7d833315eb3c8
Adam <adamgerhant@gmail.com>
2023-11-06 12:12:49 -0800

    about

--- src/about.js
+++ src/about.js
@@ -19,17 +19,21 @@ const About = () =>{
                 Reverse Assist reverses that process by allowing the user to select a transfer college, major, and articulated course, then view all source colleges with that course.
                 For example, instead of searching through dozens of documents to find a college with an articulated course, Reverse Assist directly provides the list of colleges.
                 <ReversedDiagram className='ml-[0px] my-8 w-[730px]'/>
-                The implementation for this can be broken down into 4 major steps
-                <ol className="ml-6" style={{ listStyleType: 'decimal'}} >
-                    <li className='mt-4'>Get metadata: Download college IDs, majors, and PDF document ids. Jacobtbigham's documentation on the various Assist API endpoints to extract this information was extremely helpful for this step. Check out his project here: {"\n"}
-                        <a href="https://github.com/jacobtbigham/ccc_transfers"className='mx-2 text-[#0000EE]'>github.com/jacobtbigham/ccc_transfers</a>
-                        <a href="https://www.jacobtbigham.com/transfers"className='mx-2 text-[#0000EE]'>www.jacobtbigham.com/transfers</a>
-                    </li>
-                    <li className='mt-4'>Create Database: Create GraphQL schema, integrate Python with the GraphQL API</li>
-                    <li className='mt-4'>Parse Data: Use metadata to get every PDF document (100,000+), and extract articulated courses from each PDF using a custom algorithim</li>
-                    <li className='mt-4'>Upload Data: Use GraphQL mutations to upload reformated information to database</li>
-                </ol>
-                
+                Technical information and source code is available on Github:{"\n"}
+                <a href="https://github.com/adamgerhant/reverseassist" target="_blank" rel="noopener noreferrer" className='text-[#0000EE]'>github.com/adamgerhant/reverseassist</a>
+
+                {/*
+                    The implementation for this can be broken down into 4 major steps
+                    <ol className="ml-6" style={{ listStyleType: 'decimal'}} >
+                        <li className='mt-4'>Get metadata: Download college IDs, majors, and PDF document ids. Jacobtbigham's documentation on the various Assist API endpoints to extract this information was extremely helpful for this step. Check out his project here: {"\n"}
+                            <a href="https://github.com/jacobtbigham/ccc_transfers"className='mx-2 text-[#0000EE]'>github.com/jacobtbigham/ccc_transfers</a>
+                            <a href="https://www.jacobtbigham.com/transfers"className='mx-2 text-[#0000EE]'>www.jacobtbigham.com/transfers</a>
+                        </li>
+                        <li className='mt-4'>Create Database: Create GraphQL schema, integrate Python with the GraphQL API</li>
+                        <li className='mt-4'>Parse Data: Use metadata to get every PDF document (100,000+), and extract articulated courses from each PDF using a custom algorithim</li>
+                        <li className='mt-4'>Upload Data: Use GraphQL mutations to upload reformated information to database</li>
+                    </ol>
+                */}
             </div>
         </div>
     )
